1b6d0c57356ce9eee6da288e3633886f6fab1dbc
teixeirazeus <teixeira.zeus@gmail.com>
2024-08-13 12:21:38 -0300

    feat: save and load model

--- spam_doggy.py
+++ spam_doggy.py
@@ -1,4 +1,4 @@
-from typing import Union
+import joblib
 from pandas import DataFrame
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
@@ -9,10 +9,12 @@ from datasets.data_loader import load_ashfakyeafi
 
 
 class SpamDoggy:
-    """Spam classifer that uses bayens network to classify spam emails."""
+    """Spam classifier that uses Bayesian network to classify spam emails."""
 
     def __init__(self, train_with_default_data: bool = False):
         self.df = None
+        self.vectorizer = None
+        self.classifier = None
         if train_with_default_data:
             self.set_default_training_data()
             self.train()
@@ -26,11 +28,7 @@ class SpamDoggy:
         self.df = df
 
     def set_default_training_data(self):
-        """Set the training data for the classifier.
-
-        Args:
-            df (PandasDataFrame): The training data.
-        """
+        """Set the training data for the classifier."""
         self.df = load_ashfakyeafi()
 
     def train(self, print_report: bool = False):
@@ -42,16 +40,16 @@ class SpamDoggy:
         )
 
         # Extrair características do texto
-        vectorizer = CountVectorizer()
-        X_train_counts = vectorizer.fit_transform(X_train)
-        X_test_counts = vectorizer.transform(X_test)
+        self.vectorizer = CountVectorizer()
+        X_train_counts = self.vectorizer.fit_transform(X_train)
+        X_test_counts = self.vectorizer.transform(X_test)
 
         # Treinar o classificador Naive Bayes
-        classifier = MultinomialNB()
-        classifier.fit(X_train_counts, y_train)
+        self.classifier = MultinomialNB()
+        self.classifier.fit(X_train_counts, y_train)
 
         # Avaliar o classificador
-        y_pred = classifier.predict(X_test_counts)
+        y_pred = self.classifier.predict(X_test_counts)
         accuracy = accuracy_score(y_test, y_pred)
         self.accuracy = accuracy
 
@@ -62,17 +60,25 @@ class SpamDoggy:
 
         return accuracy
 
+    def save_model(self, vectorizer_path: str = 'vectorizer.joblib', classifier_path: str = 'classifier.joblib'):
+        """Save the trained model and vectorizer to files."""
+        if self.vectorizer is None or self.classifier is None:
+            raise Exception("Model not trained. Train the model before saving.")
+        joblib.dump(self.vectorizer, vectorizer_path)
+        joblib.dump(self.classifier, classifier_path)
+
+    def load_model(self, vectorizer_path: str = 'vectorizer.joblib', classifier_path: str = 'classifier.joblib'):
+        """Load the trained model and vectorizer."""
+        self.vectorizer = joblib.load(vectorizer_path)
+        self.classifier = joblib.load(classifier_path)
+
     def predict(self, text: str):
         """Predict the class of a given text."""
-
-        # Extrair características do texto
-        vectorizer = CountVectorizer()
-        X_counts = vectorizer.fit_transform(self.df["Message"])
-
-        # Treinar o classificador Naive Bayes
-        classifier = MultinomialNB()
-        classifier.fit(X_counts, self.df["Category"])
+        
+        if self.vectorizer is None or self.classifier is None:
+            raise Exception("Model not trained or loaded. Call train() or load_model() first.")
 
         # Prever a classe do texto
-        X_text_counts = vectorizer.transform([text])
-        return classifier.predict(X_text_counts)[0]
+        X_text_counts = self.vectorizer.transform([text])
+        return self.classifier.predict(X_text_counts)[0]
+
